4fd1a0dda30ca4bdafed0f6e6390a49b6dd6bee6
brooks_yo <yasmine.olivia.brooks@live.mercer.edu>
2018-10-24 08:55:54 -0400

    Initial Midpoint File, will separate out into different files after confirmation it works

---
+++ testFile.cpp
@@ -0,0 +1,130 @@
+#include <GL/glut.h>
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//constants
+
+const float WINDOW_MAX = 1000;
+const float WINDOW_MIN = 0;
+const float VIEWPORT_MAX =900;
+const float VIEWPORT_MIN = 100;
+
+//use the midpoint circle algorithm to find the points for the circle, store them in a structure, then by trial and error decide on which one to cut off at 
+
+//struct declarations
+//Point struct
+struct Point{
+	double x, y;
+};
+
+//global variables
+vector<Point> myCircle;
+
+
+void myInit(void)
+{
+	//set black background
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glPointSize(5.0);
+	
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0.0, (float) WINDOW_MAX, 0.0, (float) WINDOW_MAX);
+	glMatrixMode(GL_MODELVIEW);
+	
+}
+
+//stores and draws point
+void plotPoint(double x, double y)
+{
+	//store the point
+	Point circPoint;
+	circPoint.x = x;
+	circPoint.y = y;
+	myCircle.push_back(circPoint);
+	
+	//draw the point
+	glColor3f(1.0, 1.0, 1.0);
+	glBegin(GL_POINTS);
+		glVertex2d(x, y);
+	glEnd();
+}
+
+//determine where the point will be in all eight octants
+void pointSymmetry(double xCord, double yCord, double x, double y)
+{
+	plotPoint(xCord + x, yCord + y);
+	plotPoint(xCord - x, yCord + y);
+	plotPoint(xCord + x, yCord - y);
+	plotPoint(xCord - x, yCord - y);
+	plotPoint(xCord + y, yCord + x);
+	plotPoint(xCord - y, yCord + x);
+	plotPoint(xCord	+ y, yCord - x);
+	plotPoint(xCord - y, yCord - x);			
+}
+
+//midpoint circle algorithm taken from pgs 150 - 153 of textbook 
+//change all to integers and convert to doubles at the end 
+void midpointCircle(double xCord, double yCord, int radius)
+{
+	xCord = (int) xCord;
+	yCord = (int) yCord;
+	int x, y;
+	x = 0;
+	y = radius;
+
+	int p = 1 - radius;
+	
+	//plotPoint(x, y);
+	pointSymmetry(xCord, yCord, x, y);
+	
+	
+	while(x < y)
+	{
+		x++;
+
+		if(p < 0)
+		{
+			p = p + 2*(x +1);
+		}
+		else
+		{
+			y--;
+			p = p + 2*(x - y) +1;
+		}
+		pointSymmetry((double) xCord, (double) yCord, (double) x, (double) y);
+	}
+
+}
+/*
+void drawTree()
+{
+
+}*/
+
+void display(void)
+{
+	//draw viewport window in white
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0, 1.0, 1.0);
+	glRectf(VIEWPORT_MIN, VIEWPORT_MIN, VIEWPORT_MAX, VIEWPORT_MAX); 
+
+	//draw the tree
+//	glColor3f(0.0f, 0.0f, 1.0f);
+//	drawTree();
+}
+
+void main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE|GLUR_RGB); //change to double later
+	glutInitWindowSize(WINDOW_MAX, WINDOW_MAX);
+	glutInitWindowPosition(0,0);
+	glutCreateWindow("Tree");
+	myInit();
+	//add mouse and keyboard functions here
+	glutDisplayFunc(display);
+	glutMainLoop();
+}
