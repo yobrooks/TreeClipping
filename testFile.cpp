78879d8ecbf26f52f4abf0ca29e4be8451d24372
brooks_yo <yasmine.olivia.brooks@live.mercer.edu>
2018-10-27 16:49:36 -0400

    Circle drawn, preliminary octants removed, vector sizes changes when window expanded???, seems to be wayyy to many points???

--- testFile.cpp
+++ testFile.cpp
@@ -4,12 +4,12 @@
 #include <vector>
 using namespace std;
 
-//constants
+//constants--MUST CHANGE BEFORE TURNING IN
 
-const float WINDOW_MAX = 1000;
-const float WINDOW_MIN = 0;
-const float VIEWPORT_MAX =900;
-const float VIEWPORT_MIN = 100;
+const int WINDOW_MAX = 600;
+const int WINDOW_MIN = 0;
+const int VIEWPORT_MAX =500;
+const int VIEWPORT_MIN = 100;
 
 //use the midpoint circle algorithm to find the points for the circle, store them in a structure, then by trial and error decide on which one to cut off at 
 
@@ -22,12 +22,21 @@ struct Point{
 //global variables
 vector<Point> myCircle;
 
+void myglutInit(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+	glutInitWindowSize(WINDOW_MAX, WINDOW_MAX);
+	glutInitWindowPosition(0,0);
+	glutCreateWindow("Tree");
+
+}
 
 void myInit(void)
 {
 	//set black background
 	glClearColor(0.0, 0.0, 0.0, 1.0);
-	glPointSize(5.0);
+	glPointSize(1.0);
 	
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
@@ -46,12 +55,13 @@ void plotPoint(double x, double y)
 	myCircle.push_back(circPoint);
 	
 	//draw the point
-	glColor3f(1.0, 1.0, 1.0);
+	glColor3f(0.0, 0.0, 1.0);
 	glBegin(GL_POINTS);
 		glVertex2d(x, y);
 	glEnd();
 }
 
+//everything in octant 5 and 6 (with starting octant being 0) needs to not be drawn...
 //determine where the point will be in all eight octants
 void pointSymmetry(double xCord, double yCord, double x, double y)
 {
@@ -60,25 +70,25 @@ void pointSymmetry(double xCord, double yCord, double x, double y)
 	plotPoint(xCord + x, yCord - y);
 	plotPoint(xCord - x, yCord - y);
 	plotPoint(xCord + y, yCord + x);
-	plotPoint(xCord - y, yCord + x);
-	plotPoint(xCord	+ y, yCord - x);
-	plotPoint(xCord - y, yCord - x);			
+	//plotPoint(xCord - y, yCord + x);
+	plotPoint(xCord+ y, yCord - x);
+	//plotPoint(xCord - y, yCord - x);			
 }
 
 //midpoint circle algorithm taken from pgs 150 - 153 of textbook 
 //change all to integers and convert to doubles at the end 
-void midpointCircle(double xCord, double yCord, int radius)
+void midpointCircle(int xCord, int yCord, int radius)
 {
-	xCord = (int) xCord;
-	yCord = (int) yCord;
-	int x, y;
+	double xCordD = (double) xCord;
+	double yCordD = (double) yCord;
+	double x, y;
 	x = 0;
-	y = radius;
+	y = (double) radius;
 
-	int p = 1 - radius;
+	double p = (5/4) - (double)radius;
 	
-	//plotPoint(x, y);
-	pointSymmetry(xCord, yCord, x, y);
+	plotPoint(x, y);
+	pointSymmetry(xCordD, yCordD, x, y);
 	
 	
 	while(x < y)
@@ -94,8 +104,11 @@ void midpointCircle(double xCord, double yCord, int radius)
 			y--;
 			p = p + 2*(x - y) +1;
 		}
-		pointSymmetry((double) xCord, (double) yCord, (double) x, (double) y);
+		pointSymmetry(xCordD, yCordD, x, y);
 	}
+
+	cout << "FINISHED" << endl;
+	//cout << myCircle.size() << endl;
 
 }
 /*
@@ -109,20 +122,25 @@ void display(void)
 	//draw viewport window in white
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(1.0, 1.0, 1.0);
-	glRectf(VIEWPORT_MIN, VIEWPORT_MIN, VIEWPORT_MAX, VIEWPORT_MAX); 
+	glRecti(VIEWPORT_MIN, VIEWPORT_MIN, VIEWPORT_MAX, VIEWPORT_MAX); 
 
+	glColor3f(0.0, 0.0, 1.0);
+	//put circle coordinate into constants 
+	midpointCircle(VIEWPORT_MAX-100, VIEWPORT_MAX-200, 50);	
 	//draw the tree
 //	glColor3f(0.0f, 0.0f, 1.0f);
 //	drawTree();
+	glFlush();
 }
 
-void main(int argc, char** argv)
+int main(int argc, char** argv)
 {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE|GLUR_RGB); //change to double later
-	glutInitWindowSize(WINDOW_MAX, WINDOW_MAX);
-	glutInitWindowPosition(0,0);
-	glutCreateWindow("Tree");
+	//glutInit(&argc, argv);
+	//glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB); //change to double later
+	//glutInitWindowSize(WINDOW_MAX, WINDOW_MAX);
+	//glutInitWindowPosition(0,0);
+	//glutCreateWindow("Tree");
+	myglutInit(argc, argv);
 	myInit();
 	//add mouse and keyboard functions here
 	glutDisplayFunc(display);
