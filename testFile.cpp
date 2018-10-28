0268e2eb7679d4ea3842c9a00432063042b3526e
brooks_yo <yasmine.olivia.brooks@live.mercer.edu>
2018-10-28 19:45:00 -0400

    transformation matrices built, keyboard and mouse(incomplete) functions added

--- testFile.cpp
+++ testFile.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
 //constants--MUST CHANGE BEFORE TURNING IN
@@ -14,22 +15,40 @@ const int VIEWPORT_MIN = 100;
 //use the midpoint circle algorithm to find the points for the circle, store them in a structure, then by trial and error decide on which one to cut off at 
 
 //struct declarations
-//Point struct
-struct Point{
-	double x, y;
+struct Vertex{
+	double x, y, z, w;
 };
+
+struct Circle{
+	int numCircPoints=0;
+	vector<Vertex> myCircPoints;
+};
+
+struct BaseTree{
+	int numBasePoints=0;
+	vector<Vertex> myBasePoints;	
+};
+
+struct Tree{
+	Circle myCircle;
+	BaseTree myBaseTree;
+};
+
+
 
 const int circleCenterX = VIEWPORT_MAX-100;
 const int circleCenterY = VIEWPORT_MAX-200;
 const int circleRadius = 60;
 
 //global variables
-vector<Point> myCircle;
+Circle circle;
+BaseTree baseTree;
+Tree myTree;
 
 void myglutInit(int argc, char** argv)
 {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB); //change to double 
 	glutInitWindowSize(WINDOW_MAX, WINDOW_MAX);
 	glutInitWindowPosition(0,0);
 	glutCreateWindow("Tree");
@@ -53,11 +72,14 @@ void myInit(void)
 void plotPoint(double x, double y)
 {
 	//store the point
-	Point circPoint;
-	circPoint.x = x;
-	circPoint.y = y;
-	myCircle.push_back(circPoint);
+	Vertex vertex;
+	vertex.x = x;
+	vertex.y = y;
+	vertex.z = 0;
+	vertex.w = 0;
 	
+	circle.myCirclePoints.push_back(vertex);
+
 	//draw the point
 	glColor3f(0.0, 0.0, 1.0);
 	glBegin(GL_POINTS);
@@ -115,11 +137,44 @@ void midpointCircle(int xCord, int yCord, int radius)
 	//cout << myCircle.size() << endl;
 
 }
-/*
+
 void drawTree()
 {
+	
+	glColor3f(0.0, 0.0, 1.0);
 
-}*/
+	midpointCircle(circleCenterX, circleCenterY, circleRadius);
+
+        glBegin(GL_LINES);
+                glVertex2i(circleCenterX+20, circleCenterY+15);
+                glVertex2i(circleCenterX-42, circleCenterY+42);
+        glEnd();
+	
+
+        glBegin(GL_LINES);
+                glVertex2i(circleCenterX+20, circleCenterY-15);
+                glVertex2i(circleCenterX-42, circleCenterY-42);
+        glEnd();
+
+        glBegin(GL_LINES);
+                glVertex2i(circleCenterX+20, circleCenterY+15);
+                glVertex2i(VIEWPORT_MIN+100, VIEWPORT_MAX-175);
+        glEnd();
+
+        glBegin(GL_LINES);
+                glVertex2i(circleCenterX+20, circleCenterY-15);
+                glVertex2i(VIEWPORT_MIN+100, VIEWPORT_MIN+160);
+        glEnd();
+
+        glBegin(GL_LINES);
+                glVertex2i(VIEWPORT_MIN+100, VIEWPORT_MAX-175);
+                glVertex2i(VIEWPORT_MIN+100, VIEWPORT_MIN+160);
+        glEnd();
+
+	glFlush();
+
+	//add circle and Base Tree to Tree 		
+}
 
 void display(void)
 {
@@ -127,46 +182,92 @@ void display(void)
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(1.0, 1.0, 1.0);
 	glRecti(VIEWPORT_MIN, VIEWPORT_MIN, VIEWPORT_MAX, VIEWPORT_MAX); 
-
-	glColor3f(0.0, 0.0, 1.0);
-	//put circle coordinate into constants 
-	midpointCircle(circleCenterX, circleCenterY, circleRadius);	
-	//draw the tree
-	glColor3f(0.0, 0.0, 1.0);
-	glBegin(GL_LINES);
-		glVertex2i(circleCenterX+20, circleCenterY+15);
-		glVertex2i(circleCenterX-42, circleCenterY+42);
-	glEnd();
 	
-	glBegin(GL_LINES);
-		glVertex2i(circleCenterX+20, circleCenterY-15);
-		glVertex2i(circleCenterX-42, circleCenterY-42);
-	glEnd();
+	drawTree();
+}
 
-	glBegin(GL_LINES);
-		glVertex2i(circleCenterX+20, circleCenterY+15);
-		glVertex2i(VIEWPORT_MIN+100, VIEWPORT_MAX-175);
-	glEnd();
+//build translation matrix
+void buildTranslateMatrix(vector<double> translateVec, double x, double y, double z)
+{
+	translateVec.push_back(1.0);	translateVec.push_back(0.0);	translateVec.push_back(0.0); 	translateVec.push_back(x);
+	translateVec.push_back(0.0); 	translateVec.push_back(1.0); 	translateVec.push_back(0.0);	translateVec.push_back(y);
+	translateVec.push_back(0.0);	translateVec.push_back(0.0);	traslateVec.push_back(1.0);	translateVec.push_back(z);	
+	translateVec.push_back(0.0);	translateVec.push_back(0.0);	translateVec.push_back(0.0);	translateVec.push_back(1.0);
+}
 
-	glBegin(GL_LINES);
-		glVertex2i(circleCenterX+20, circleCenterY-15);
-		glVertex2i(VIEWPORT_MIN+100, VIEWPORT_MIN+160);
-	glEnd();
+//build rotation matrix that does a rotation about the z axis; based on angle passed in
+void buildRotationMatrix(double thetha, vector<double> rotateVec)
+{
+	double angle = (theta*M_PI)/180.0; //convert angle to radians
 	
-	glBegin(GL_LINES);
-		glVertex2i(VIEWPORT_MIN+100, VIEWPORT_MAX-175);
-		glVertex2i(VIEWPORT_MIN+100, VIEWPORT_MIN+160);
-	glEnd();
+	rotateVec.push_back(cos(angle));   rotateVec.push_back(-sin(angle));	rotateVec.push_back(0.0);	rotateVec.push_back(0.0);
+	rotateVec.push_back(sin(angle));   rotateVec.push_back(cos(angle));	rotateVec.push_back(0.0);	rotateVec.push_back(0.0);
+	rotateVec.push_back(0.0);	   rotateVec.push_back(0.0);		rotateVec.push_back(1.0);	rotateVec.push_back(0.0);
+	rotateVec.push_back(0.0);	   rotateVec.push_back(0.0);		rotateVec.push_back(0.0);	rotateVec.push_back(1.0); 
+}
+
+//build Scale Matrix; based on scale factor passed in
+void buildScaleMatrix(double increaseScale, vector<double> scaleVec)
+{
+	//make scale into a decimal from percentage
+	//increaseScale = increaseScale/100;
 	
-//	drawTree();
-	glFlush();
+	scaleVec.push_back(increaseScale);	scaleVec.push_back(0.0);	scaleVec.push_back(0.0);	scaleVec.push_back(0.0);
+	scaleVec.push_back(0.0);	scaleVec.push_back(increaseScale);	scaleVec.push_back(0.0);	scaleVec.push_back(0.0);	   scaleVec.push_back(0.0);		   scaleVec.push_back(0.0);	 scaleVec.push_back(increaseScale); scaleVec.push_back(0.0);
+	scaleVec.push_back(0.0);		scaleVec.push_back(0.0);	scaleVec.push_back(0.0);	scaleVec.push_back(1.0);
+}
+
+void mouse(int button, int state, int x, int y)
+{
+	//if mouse inside of viewport
+	if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	{
+		//increase speed of rotation
+	}
+
+	if(button == GLUT_RIGHT_DOWN && state == GLUT_DOWN)
+	{
+		//slow down rotation
+	}
+
+	//else if mouse is outside viewport
+	if(button == GLUT_LEFT_DOWN && state == GLUT_DOWN)
+	{
+		//increase scale of tree
+	}
+	
+	if(button == GLUT_RIGHT_DOWN && state == GLUT_DOWN)
+	{
+		//decrease scale of tree
+	}		
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+	switch(tolower(key))
+	{
+		case 'q' : exit(0);
+		case 'f' : //fill polygon
+			break;
+		case 't' : //tesselation triangles
+			break;
+		case 'l' : //back to initial outline
+			break;
+		case 'r' : //reflect
+			break;
+		case 's' : //stop animation
+			break;
+		case 'i' : //stop animation, return to original position
+			break;
+	}
 }
 
 int main(int argc, char** argv)
 {
 	myglutInit(argc, argv);
 	myInit();
-	//add mouse and keyboard functions here
+	glutMouseFunc(mouse);
+	glutKeyboardFunc(keyboard);
 	glutDisplayFunc(display);
 	glutMainLoop();
 }
